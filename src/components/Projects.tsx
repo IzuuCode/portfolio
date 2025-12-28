5bc5761f639ebb3f910e807703cab68c73446e85
Isuru  Jayanada <137614712+IzuuCode@users.noreply.github.com>
2025-12-28 16:33:21 +0530

    Update Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -44,7 +44,7 @@ const projects = [
     live: 'https://github.com/IzuuCode/flappy_Bird'
   },
   {
-    title: 'Personal Portafoliot',
+    title: 'Personal Portafolio',
     description: 'Welcome to the repository for my personal website — a digital space where I showcase my projects, technical skills, and share insights from my learning journey. Feel free to explore and get to know more about me',
     image: '👦🏼',
     tech: ['React', 'Javascript', 'Email.js', 'Typescript'],
